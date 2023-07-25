84bd00b19ba285d6f6ce7905c1b50c81ca4183c0
Justin Hille <jhille@f1r4s11.codam.nl>
2023-07-25 17:43:51 +0200

    working on new user form

--- frontend/volume/src/components/login/NewUser.tsx
+++ frontend/volume/src/components/login/NewUser.tsx
@@ -2,13 +2,19 @@ import { useQuery } from "@apollo/client";
 import "src/styles/login-pages/new-user.css";
 import { CURRENT_USER } from "src/utils/graphQLQueries";
 import Loading from "../authorization/Loading";
-import ProfileForm from "../settings/ProfileForm";
+import NewUserForm from "./NewUserForm";
 
 function NewUser(): JSX.Element {
 	const { loading, error, data } = useQuery(CURRENT_USER);
 
 	if (loading) return <Loading />;
-	if (error) return <>Error</>;
+	if (error) {
+		if (gqlErrorCode(error) == "UNAUTHENTICATED") {
+			return <Navigate to="/login" />;
+		} else {
+			return <Error gqlError={error} />;
+		}
+	}
 
 	return (
 		<div className="background">
@@ -18,7 +24,7 @@ function NewUser(): JSX.Element {
 						<h1>Welcome to PONG</h1>
 						please fill create a username and select a profile picture
 					</div>
-					<ProfileForm user={data.currentUserQuery} />
+					<NewUserForm user={data.currentUserQuery} />
 				</div>
 			</div>
 		</div>
