bd864b9f7dcef7736e64034accdc1492839940a1
Jonathan Bedaux <83814959+mo-bedo@users.noreply.github.com>
2023-08-10 14:20:46 +0200

    Friend requests fixed so outgoing is not linked with incoming

--- backend/volume/src/user/entities/user.entity.ts
+++ backend/volume/src/user/entities/user.entity.ts
@@ -70,12 +70,12 @@ export class User {
 	@JoinTable()
 	friends: User[];
 
-	@ManyToMany(() => User, (user) => user.outgoing_friend_requests)
+	@ManyToMany(() => User, (user) => user.incoming_friend_requests)
 	@JoinTable()
 	@Field(() => [User])
 	incoming_friend_requests: User[];
 
-	@ManyToMany(() => User, (user) => user.incoming_friend_requests)
+	@ManyToMany(() => User, (user) => user.outgoing_friend_requests)
 	@JoinTable()
 	@Field(() => [User])
 	outgoing_friend_requests: User[];
